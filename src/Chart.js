35a8465317d41d9f70420b371ddea6ba6ce220e1
aaditya-thakkar <thakkar.aadi1@gmail.com>
2016-06-28 23:00:10 +0530

    removed unnecessary methods

--- src/Chart.js
+++ src/Chart.js
@@ -8,6 +8,7 @@ var helper = require("./helper");
 var countryArray = [];
 var codeArray = [];
 var appbaseRef = helper.createAppbaseRef();
+
 for(var i=0; i<countryCode.length; i++){
   countryArray[countryCode[i].code.toLowerCase()] = countryCode[i].name;
   codeArray[countryCode[i].name] = countryCode[i].code.toLowerCase();
@@ -18,14 +19,14 @@ var Chart = React.createClass({
       data : {
         labels: ["001", "002", "003", "004", "005", "006", "007"],
         datasets: [
-        {
-          label: "My First dataset",
-          fillColor: "rgba(220,0,0,0.5)",
-          strokeColor: "rgba(220,0,0,0.8)",
-          highlightFill: "rgba(220,0,220,0.75)",
-          highlightStroke: "rgba(220,0,220,1)",
-          data: [20, 59, 80, 81, 56, 55, 40]
-        }
+          {
+            label: "My First dataset",
+            fillColor: "rgba(220,0,0,0.5)",
+            strokeColor: "rgba(220,0,0,0.8)",
+            highlightFill: "rgba(220,0,220,0.75)",
+            highlightStroke: "rgba(220,0,220,1)",
+            data: [20, 59, 80, 81, 56, 55, 40]
+          }
         ]
       },
       options: {
@@ -40,10 +41,6 @@ var Chart = React.createClass({
     });
   },
 
-  getRandomNumber: function(){
-    return Math.floor((Math.random() * 100) + 1);
-  },
-
   callStaticUpdates: function(tempData, requestObject) {
     var resultArr = [];
     var self = this;
@@ -53,7 +50,6 @@ var Chart = React.createClass({
         var indexOfCountry = tempData.labels.indexOf(countryArray[bucket.key]);
         tempData.datasets[0].data[indexOfCountry] = bucket.doc_count;
       });
-
       self.setState({
         data:tempData
       });
@@ -65,8 +61,6 @@ var Chart = React.createClass({
   onFilterUpdate: function(val) {
     var countryArr = [];
     var tempData=this.state.data;
-    // console.log(codeArray);
-
     var updatedLabels = val.split(",",500)
     for(var i=0;i<updatedLabels.length;i++){
       console.log(updatedLabels[i]);
@@ -83,17 +77,13 @@ var Chart = React.createClass({
     this.callStaticUpdates(tempData, requestObject);
   },
 
-  componentDidMount: function(){
-
-  },
-
   render: function() {
     return(<div>
       <Filter updateFilter={this.onFilterUpdate} />
       <LineChart data={this.state.data} options={this.state.options} width="800" height="350" />
       </div>
-      );
-    }
-  });
+    );
+  }
+});
 
-  module.exports = Chart;
+module.exports = Chart;
